9f737fb3f3f2105ccfe8da03d8871d0322bd98af
Akshaya2007 <81800055+Akshaya2007@users.noreply.github.com>
2025-04-17 16:08:06 +0545

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ async function generateQR() {
     return;
   }
 
-  const response = await fetch('https://dynqrdemo.onrender.com', {
+  const response = await fetch('https://dynqrdemo.onrender.com/generateQR', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ amount })
